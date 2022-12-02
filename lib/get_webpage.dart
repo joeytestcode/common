a9944f99073fe85bf9e7007e1963dcf4f7e8ae26
joeyhwangGit <jjoeyhwang@gmail.com>
2022-12-02 23:01:15 +0900

    20221202_2301

--- lib/get_webpage.dart
+++ lib/get_webpage.dart
@@ -1,14 +1,20 @@
-import 'package:dio/dio.dart';
+import 'package:html/dom.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart';
 
 class GetWebpage {
   static const _maxTry = 4;
-  static Future<Response> get(String page) async {
-    Response response;
+  static Future<http.Response> get(String page) async {
+    http.Response response;
     int nTry = 0;
     do {
-      response = await Dio().get(page);
+      response = await http.get(Uri.parse(page));
       nTry++;
     } while (response.statusCode != 200 && nTry < _maxTry);
     return response;
+  }
+
+  static Future<Document> getDocument(String page) async {
+    return parse((await get(page)));
   }
 }
