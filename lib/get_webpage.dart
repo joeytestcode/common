e390c982c321844bf87444db2883a80188d19d76
joeyhwangGit <jjoeyhwang@gmail.com>
2022-12-04 17:59:14 +0900

    202212041759

--- lib/get_webpage.dart
+++ lib/get_webpage.dart
@@ -1,21 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:html/dom.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart';
 
 class GetWebpage {
+  static const _corsProxy = 'https://cors-anywhere.herokuapp.com/';
   static const _maxTry = 4;
   static Future<http.Response> get(String page) async {
     http.Response response;
     int nTry = 0;
     do {
-      response = await http.get(
-        Uri.parse(page),
-        headers: {
-          'Access-Control-Allow-Origin': 'https://joeytestcode.github.io',
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
-        },
-      );
+      response = await http
+          .get(kIsWeb ? Uri.parse(_corsProxy + page) : Uri.parse(page));
       nTry++;
     } while (response.statusCode != 200 && nTry < _maxTry);
     return response;
