6d5f35756ec202d179335015ea34835315d44d91
Joey Hwang <jjoeyhwang@gmail.com>
2023-01-01 02:54:51 +0900

    202301010254

--- lib/get_webpage.dart
+++ lib/get_webpage.dart
@@ -40,33 +40,3 @@ class GetWeb {
     return directory.path;
   }
 }
-
-class GetWebPage {
-  static const _corsProxy = 'https://corsproxy-371208.du.r.appspot.com/';
-  static const _maxTry = 4;
-  static Future<http.Response> get(String page) async {
-    http.Response response;
-    int nTry = 0;
-    do {
-      response = await http
-          .get(kIsWeb ? Uri.parse(_corsProxy + page) : Uri.parse(page));
-      nTry++;
-    } while (response.statusCode != 200 && nTry < _maxTry);
-    return response;
-  }
-
-  static Future<Document> getDocument(String page) async {
-    return parse((await get(page)).body);
-  }
-
-  static Future<File> getFile(String webSite, {String fileName = ''}) async {
-    if (fileName.isEmpty) {
-      fileName = RegExp(r'([^\/]+$)').firstMatch(webSite)?.group(1) ?? 'temp';
-    }
-    final request = await HttpClient().getUrl(Uri.parse(webSite));
-    final response = await request.close();
-    final File file = File(fileName);
-    await response.pipe(file.openWrite());
-    return file;
-  }
-}
