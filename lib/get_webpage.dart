8eb844c8b938473b6c6ace8d55f9cd05ba754449
joeyhwangGit <jjoeyhwang@gmail.com>
2023-01-01 01:25:00 +0900

    202301010124

--- lib/get_webpage.dart
+++ lib/get_webpage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:html/dom.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart';
+import 'package:path_provider/path_provider.dart';
 
 class GetWeb {
   static const _corsProxy = 'https://corsproxy-371208.du.r.appspot.com/';
@@ -29,9 +30,14 @@ class GetWeb {
     }
     final request = await HttpClient().getUrl(Uri.parse(webSite));
     final response = await request.close();
-    final File file = File(fileName);
+    final File file = File('${await _localPath}/$fileName');
     await response.pipe(file.openWrite());
     return file;
+  }
+
+  static Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
   }
 }
 
